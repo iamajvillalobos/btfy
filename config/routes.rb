8de65ab3ec362106315fa9696eea47479381de77
AJ Villalobos <hi@ajvillalobos.com>
2020-07-27 20:20:30 +0800

    Add sidekiq & redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
 	root to: "links#index"
 
   resources :custom_domains
   resources :links
+
+	authenticate :user do |user|
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   get "/:slug", to: "redirect_links#new", as: :redirect
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
