a651affb4c43945ed9e44e3fdfa9f5779169344b
AJ Villalobos <hi@ajvillalobos.com>
2020-07-28 12:41:48 +0800

    Pass user email to paddle button

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+	static targets = ["user"]
+
+	initialize() {
+		this.user;
+	}
+
 	connect() {
-		console.log("hello");
+		Paddle.Setup({ vendor: 118869 });
+
+		this.userTarget.style.display = "none";
+		this.user = JSON.parse(this.userTarget.textContent)
+	}
+
+	subscribe() {
+		Paddle.Checkout.open({
+			product: 610658,
+			email: this.user.email,
+		})
 	}
 }
