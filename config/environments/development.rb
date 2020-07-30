09bcc80cbcdc01db79452116eb807185b26223b9
AJ Villalobos <hi@ajvillalobos.com>
2020-07-30 13:48:28 +0800

    feat: add more host for dev testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.hosts << "s.ajvillalobos.com"
+  config.hosts << "s.btfy.io"
 end
