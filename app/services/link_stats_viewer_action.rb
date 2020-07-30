5b8f1c82c912ea0eaabf0fe200cfb7224b835ffc
AJ Villalobos <hi@ajvillalobos.com>
2020-07-30 22:23:48 +0800

    style: improve graph design

--- app/services/link_stats_viewer_action.rb
+++ app/services/link_stats_viewer_action.rb
@@ -10,15 +10,22 @@ class LinkStatsViewerAction
 		:countries
 
 	executed do |ctx|
+		range = Time.now.last_month..Time.now
 		events = Ahoy::Event.where_props(link_id: ctx.link.id)
 		uniq_events = Ahoy::Event.where(id: events.uniq(&:visit_id).map(&:id))
 
 		ctx.unique_visit_count = events.distinct.pluck(:visit_id).count
-		ctx.unique_visit_count_grouped = uniq_events.group_by_day(:time).count
 		ctx.visit_count = events.pluck(:visit_id).count
-		ctx.visit_count_grouped = events.group_by_day(:time).count
 		ctx.devices = uniq_events.map { |e| e.visit.device_type }.tally
 		ctx.browsers = uniq_events.map { |e| e.visit.browser }.tally
 		ctx.countries = uniq_events.map { |e| e.visit.country }.tally
+
+		ctx.visit_count_grouped = events.group_by_day_of_month(
+			:time, range: range, series: false
+		).count
+
+		ctx.unique_visit_count_grouped = uniq_events.group_by_day_of_month(
+			:time, range: range, series: false
+		).count
 	end
 end
