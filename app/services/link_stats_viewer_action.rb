06188b968ee772b7dca04b6588e1808a18b0eb91
AJ Villalobos <hi@ajvillalobos.com>
2020-07-30 17:13:16 +0800

    feat: add more stats for LinkStatsViewerAction

--- app/services/link_stats_viewer_action.rb
+++ app/services/link_stats_viewer_action.rb
@@ -1,12 +1,15 @@
 class LinkStatsViewerAction
 	include LightService::Action
 	expects :link
-	promises :visit_count
+	promises :unique_visit_count, :visit_count, :devices, :browsers, :countries
 
 	executed do |ctx|
-		ctx.visit_count = Ahoy::Event.where_props(link_id: ctx.link.id)
-			.distinct
-			.pluck(:visit_id)
-			.count
+		events = Ahoy::Event.where_props(link_id: ctx.link.id)
+
+		ctx.unique_visit_count = events.distinct.pluck(:visit_id).count
+		ctx.visit_count = events.pluck(:visit_id).count
+		ctx.devices = events.map { |e| e.visit.device_type }.tally
+		ctx.browsers = events.map { |e| e.visit.browser }.tally
+		ctx.countries = events.map { |e| e.visit.country }.tally
 	end
 end
