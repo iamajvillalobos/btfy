be1d8af7e6a49e849a78ea768a63599156418f72
AJ Villalobos <hi@ajvillalobos.com>
2020-07-31 16:06:33 +0800

    build: setup automatic code formatting for ruby & js

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 
   has_many :links, dependent: :destroy
   has_many :custom_domains, dependent: :destroy
