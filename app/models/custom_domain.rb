68106331c81787037a905e33448fcd47f512d6cd
AJ Villalobos <hi@ajvillalobos.com>
2020-08-15 04:15:00 +0800

    feat: delete related links when you delete a domain

--- app/models/custom_domain.rb
+++ app/models/custom_domain.rb
@@ -2,6 +2,6 @@ class CustomDomain < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  has_many :links, dependent: :nullify
+  has_many :links, dependent: :destroy
   belongs_to :user
 end
