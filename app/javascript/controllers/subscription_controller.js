b0546730a0e825c4ef7bb6cc1470db5f16ab9be3
AJ Villalobos <hi@ajvillalobos.com>
2020-08-16 17:47:53 +0800

    feat: create subscription when a user subscribe

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
 	initialize() {
-		this.user = JSON.parse(this.data.get("user"))
+		this.user = JSON.parse(this.data.get("user"));
 	}
 
 	connect() {
@@ -13,7 +13,20 @@ export default class extends Controller {
 		const product = event.currentTarget.dataset.product;
 		Paddle.Checkout.open({
 			product: product,
-			email: this.user,
-		})
+			email: this.user.email,
+			passthrough: JSON.stringify({
+				owner_id: this.user.id,
+				owner_type: "User",
+			}),
+			successCallback: this.checkoutComplete.bind(this),
+		});
+	}
+
+	checkoutComplete(data) {
+		let isCompleted = data.checkout.completed;
+
+		if (isCompleted) {
+			location.href = this.data.get("billing-path");
+		}
 	}
 }
