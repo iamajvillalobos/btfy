e96882873fe5c1f28bd35a06a6c5a5250849b558
AJ Villalobos <hi@ajvillalobos.com>
2020-08-17 03:43:06 +0800

    feat: redirect user to plans when it doesn't have yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	skip_before_action :track_ahoy_visit
 
   rescue_from CanCan::AccessDenied do
-    redirect_to main_app.root_url,
+    redirect_to user_plans_url,
     notice: "You must subscribe to plan to do this action"
   end
 end
