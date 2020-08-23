b7d1b694c12051e292aa12db580d789c174d3b88
AJ Villalobos <hi@ajvillalobos.com>
2020-08-23 21:36:01 +0800

    feat: disable rollbar in development

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,8 +4,8 @@ Rollbar.configure do |config|
 
   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
+  # Disable in development and test
+  if Rails.env.test? || Rails.env.development?
     config.enabled = false
   end
 
