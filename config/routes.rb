66a05227723ddd96598beb280fcdd65339325325
AJ Villalobos <hi@ajvillalobos.com>
2020-08-23 21:47:43 +0800

    feat: create admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  devise_for :admin_users
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_172317) do
+ActiveRecord::Schema.define(version: 2020_08_23_133423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
 
   create_table "ahoy_events", force: :cascade do |t|
     t.bigint "visit_id"
