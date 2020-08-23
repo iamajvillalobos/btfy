151a93c596d21d88e9465578c4d51386b3c01fbf
AJ Villalobos <hi@ajvillalobos.com>
2020-08-23 21:45:15 +0800

    feat: add users and links in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Link.destroy_all
+CustomDomain.destroy_all
+
+# Create user
+user = User.create!(email: "test@example.com", password: "password", password_confirmation: "password")
+
+# Create links
+100.times do |i|
+  slug = Faker::Internet.slug
+
+  user.links.create!(
+    name: slug,
+    destination_url: Faker::Internet.url,
+    slug: slug
+  )
+end
