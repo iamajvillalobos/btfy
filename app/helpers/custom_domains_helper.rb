69f3a5f148535e30dd40deb6d763a1c2138bd595
AJ Villalobos <hi@ajvillalobos.com>
2020-08-24 15:35:59 +0800

    feat: enable use of btfy.io even after adding a custom domain

--- app/helpers/custom_domains_helper.rb
+++ app/helpers/custom_domains_helper.rb
@@ -1,2 +1,7 @@
 module CustomDomainsHelper
+  def custom_domain_dropdown
+    domains = current_user.custom_domains.map { |cd| [cd.name, cd.id] }
+    domains << [ENV["DEFAULT_URL_HOST"], nil]
+    domains.sort
+  end
 end
