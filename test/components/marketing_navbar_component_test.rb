7dc4a4845a16bc3fb297052aaa8e07831872c8af
AJ Villalobos <hi@ajvillalobos.com>
2020-08-25 02:29:30 +0800

    feat: add MarketingNavbarComponent

---
+++ test/components/marketing_navbar_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class MarketingNavbarComponentTest < ViewComponent::TestCase
+  test "component renders something useful" do
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(MarketingNavbarComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
