f92bd4fffa51fdd452dedb4b58015f284095e038
AJ Villalobos <hi@ajvillalobos.com>
2020-08-26 03:10:47 +0800

    feat: Move footer to MarketingFooterComponent

---
+++ test/components/marketing_footer_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class MarketingFooterComponentTest < ViewComponent::TestCase
+  test "component renders something useful" do
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(MarketingFooterComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
