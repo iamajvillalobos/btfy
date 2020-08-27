02d672652e104cad213fea3638b71192332989bb
AJ Villalobos <hi@ajvillalobos.com>
2020-08-27 20:14:18 +0800

    feat: only alphanumeric characters are allowed in slug

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ApplicationRecord
   validates :destination_url, url: true
   validate :unique_slug_per_custom_domain
+  validates_format_of :slug, with: /\A[a-zA-Z0-9]*\z/, message: "must only be numbers and letters"
 
   belongs_to :custom_domain, optional: true
   belongs_to :user
