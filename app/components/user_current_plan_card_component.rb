8ecd446a7608be03492f5770ad2ca5fca8f6d5c7
AJ Villalobos <hi@ajvillalobos.com>
2020-08-28 18:18:07 +0800

    feat: Update UserCurrentPlanCardComponent to optionally show the upgrade button

--- app/components/user_current_plan_card_component.rb
+++ app/components/user_current_plan_card_component.rb
@@ -1,5 +1,6 @@
 class UserCurrentPlanCardComponent < ViewComponent::Base
-  def initialize(user:)
+  def initialize(user:, show_button:)
     @user = user
+    @show_button = show_button
   end
 end
