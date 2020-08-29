73a70ea7f1ccd1eccc28767b5188d3ccae6cb575
AJ Villalobos <hi@ajvillalobos.com>
2020-08-29 12:05:38 +0800

    style: formatted with prettier

--- app/javascript/controllers/domains_controller.js
+++ app/javascript/controllers/domains_controller.js
@@ -4,8 +4,8 @@ import List from "list.js";
 export default class extends Controller {
   connect() {
     var options = {
-      valueNames: [ "name "]
-    }
+      valueNames: ["name "],
+    };
 
     var domainList = new List("domains", options);
   }
