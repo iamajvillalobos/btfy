5af2e0f602554f97025dfc7242a90063d272001d
AJ Villalobos <hi@ajvillalobos.com>
2020-09-02 01:28:32 +0800

    feat: enable updating of account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,9 +4,21 @@ class AccountsController < ApplicationController
   layout "dashboard"
 
   def show
-    @profile = Account.find_or_create_by(user: current_user)
+    @account = Account.find_or_create_by(user: current_user)
   end
 
   def update
+    account = current_user.account
+
+    if account.update(account_params)
+      flash[:notice] = "Profile update successfully"
+      redirect_to account_path
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:first_name, :last_name, :bio)
   end
 end
