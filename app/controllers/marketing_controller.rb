1036bee7a28c3d802e58ec71418d5f5ef4fcaa4f
AJ Villalobos <hi@ajvillalobos.com>
2020-09-04 19:41:11 +0800

    feat: redirect user to when visiting a base url connected to btfy

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,4 +1,15 @@
 class MarketingController < ApplicationController
   def index
+    if request.host != ENV["DEFAULT_URL_HOST"]
+      if custom_domain = CustomDomain.find_by(name: request.host)
+        if custom_domain.redirect_url
+          redirect_to custom_domain.redirect_url
+        else
+          render "errors/connected_domain", layout: "errors", locals: { domain: custom_domain}
+        end
+      else
+        render "marketing/index"
+      end
+    end
   end
 end
