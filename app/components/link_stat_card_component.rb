86c4b7789530ce01b7bd6934c79f87ea329d0e41
AJ Villalobos <hi@ajvillalobos.com>
2020-09-07 21:44:12 +0800

    style: use total stat table for showing stats in link

--- app/components/link_stat_card_component.rb
+++
@@ -1,8 +0,0 @@
-class LinkStatCardComponent < ViewComponent::Base
-  VIEW_LIMIT = 10
-
-  def initialize(stats:, label:)
-    @stats = stats.sort_by { |k,v| v }.reverse.first(VIEW_LIMIT)
-    @label = label
-  end
-end
