2f565b1fbfbaea81d64e5aa6bec9af3df890552f
AJ Villalobos <hi@ajvillalobos.com>
2020-09-16 19:30:14 +0800

    chore: Remove paddle & billing for now

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   # Settings
   resource :account, only: [:show, :update]
   resource :user_attributes, only: [:update]
-  resource :billing, only: [:show]
+  # resource :billing, only: [:show]
   resource :bio, only: [:show]
   resources :api_keys, only: [:index, :create, :edit, :update] do
     post :reactivate
