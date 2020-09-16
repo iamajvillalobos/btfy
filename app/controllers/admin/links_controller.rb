e315a0601ae3e5a0c2b06dcbc147bda32d5a3acf
AJ Villalobos <hi@ajvillalobos.com>
2020-09-16 13:58:57 +0800

    feat: Add Top links and Visit Chart in admin dashboard

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -4,6 +4,7 @@ class Admin::LinksController < ApplicationController
   before_action :authenticate_admin_user!
 
   def index
-    @links = Link.order(created_at: :desc)
+    @links = Link.order(created_at: :desc).first(20)
+    @stats = GroupLinkStatsViewer.call(Link.all, "30d")
   end
 end
