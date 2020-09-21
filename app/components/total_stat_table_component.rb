2eb096d15f80c81d52eb6a2625227832ffbd2ef7
AJ Villalobos <hi@ajvillalobos.com>
2020-09-21 13:43:38 +0800

    fix: don't show stats with nil keys

--- app/components/total_stat_table_component.rb
+++ app/components/total_stat_table_component.rb
@@ -2,7 +2,10 @@ class TotalStatTableComponent < ViewComponent::Base
   VIEW_LIMIT = 5
 
   def initialize(stats:, label:)
-    @stats = stats.sort_by { |k,v| v }.reverse.first(VIEW_LIMIT)
+    @stats = stats.reject { |k, v| k.nil? }
+      .sort_by { |k, v| v }
+      .reverse
+      .first(VIEW_LIMIT)
     @label = label
   end
 
