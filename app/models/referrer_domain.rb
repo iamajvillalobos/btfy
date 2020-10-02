b60d124d51a3075111740b72a989c821677830d6
AJ Villalobos <hi@ajvillalobos.com>
2020-10-03 01:15:11 +0800

    fix: use the url.path if url.host is not found

--- app/models/referrer_domain.rb
+++ app/models/referrer_domain.rb
@@ -12,12 +12,17 @@ class ReferrerDomain
   attr_reader :name
 
   def initialize(url:)
-    host = Addressable::URI.parse(url)&.host
+    host = host_or_path(url)
     @name = host ? parse_domain(host) : "Direct / None"
   end
 
   def parse_domain(url)
     return url unless URL_TO_DOMAIN.keys.include?(url)
     URL_TO_DOMAIN[url]
+  end
+
+  def host_or_path(url)
+    url = Addressable::URI.parse(url)
+    url.host || url.path.split("/").first
   end
 end
--- spec/actions/parse_visitor_referrer_spec.rb
+++ spec/actions/parse_visitor_referrer_spec.rb
@@ -13,5 +13,15 @@ module Actions
       result = described_class.execute(request: request)
       expect(result.referrer).to eq "disq.us"
     end
+
+    context "fb.me" do
+      let(:request) do
+        double("request", referrer: "fb.me/123456")
+      end
+      it "returns a string of fb.me" do
+        result = described_class.execute(request: request)
+        expect(result.referrer).to eq "fb.me"
+      end
+    end
   end
 end
