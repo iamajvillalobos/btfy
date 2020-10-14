531a79f02a6525dc22a63ec0f5a531c90500aeb2
AJ Villalobos <hi@ajvillalobos.com>
2020-10-14 16:22:59 +0800

    fix: require resolv library separately to avoid problem in prod

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 require "view_component/engine"
 require 'rails/all'
+require "resolv"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
