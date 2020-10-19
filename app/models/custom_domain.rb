7a7d120d1d91412eef31030fe82912424a2d66e4
AJ Villalobos <hi@ajvillalobos.com>
2020-10-19 12:17:45 +0800

    feat: require not_found_url field in custom domains

--- app/models/custom_domain.rb
+++ app/models/custom_domain.rb
@@ -2,6 +2,7 @@ class CustomDomain < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
   validates :redirect_url, url: true
+  validates :not_found_url, url: true
 
   has_many :links, dependent: :destroy
   belongs_to :user
