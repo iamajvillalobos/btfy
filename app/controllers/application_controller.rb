033a411a96b6a633c838a973abda1a8ea1a27c8e
AJ Villalobos <hi@ajvillalobos.com>
2020-11-05 03:18:34 +0800

    fix: remove user context for raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < ActionController::Base
   end
 
   def set_raven_context
-    Raven.user_context(id: current_user.id)
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
