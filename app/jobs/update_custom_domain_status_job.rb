d34b931cc633daa77a007ad59db918c7317c16d0
AJ Villalobos <hi@ajvillalobos.com>
2020-11-08 16:55:15 +0800

    fix: dont' add retry option

--- app/jobs/update_custom_domain_status_job.rb
+++ app/jobs/update_custom_domain_status_job.rb
@@ -1,20 +1,19 @@
 class UpdateCustomDomainStatusJob < ApplicationJob
   queue_as :default
-  sidekiq_options retry: false
 
   ACM_STATUS = {
     "cert issued" => "success",
     "in progress" => "pending",
     "dns verified" => "pending",
     "waiting" => "pending",
-    "failing" => "pending",
+    "failing" => "pending"
   }
 
   def perform
     domains = CustomDomain.pending
 
     domains.each do |domain|
-      status = CheckDnsStatus.(domain)
+      status = CheckDnsStatus.call(domain)
       puts "status: #{status}"
       domain.update(status: ACM_STATUS[status])
     end
