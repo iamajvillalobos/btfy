01ba92183c631af53d03ba7763eeebe3052f8b7e
AJ Villalobos <hi@ajvillalobos.com>
2020-11-08 16:28:04 +0800

    feat: refactor CheckDnsStatus to use PlatformAPI

--- app/services/check_dns_status.rb
+++ app/services/check_dns_status.rb
@@ -1,19 +1,8 @@
 class CheckDnsStatus
   def self.call(domain)
-    url = "https://api.heroku.com/apps/#{ENV["HEROKU_APP_NAME"]}/domains/#{domain.name}"
-    options = {
-      body: {
-        hostname: domain.name
-      },
-      headers: {
-        "Accept": "application/vnd.heroku+json; version=3",
-        "Authorization": "Basic #{ENV["HEROKU_API_KEY"]}"
-      }
-    }
-    response = HTTParty.get(url, options)
-    return unless response.success?
-
-    dns_response = OpenStruct.new(response.parsed_response)
+    heroku = PlatformAPI.connect_oauth(ENV["HEROKU_TOKEN"])
+    response = heroku.domain.info(ENV["HEROKU_APP_NAME"], domain.name)
+    dns_response = OpenStruct.new(response)
     dns_response.acm_status
   end
 end
