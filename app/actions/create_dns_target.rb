55c1b2d37318e5ce4ca72c068a83e3f56465ca63
AJ Villalobos <hi@ajvillalobos.com>
2020-11-08 16:27:55 +0800

    feat: refactor CreateDnsTarget to use PlatformAPI

--- app/actions/create_dns_target.rb
+++ app/actions/create_dns_target.rb
@@ -6,23 +6,12 @@ class CreateDnsTarget
   expects :domain
 
   executed do |ctx|
-    url = "https://api.heroku.com/apps/#{ENV["HEROKU_APP_NAME"]}/domains"
-    options = {
-      body: {
-        hostname: ctx.domain.name
-      },
-      headers: {
-        "Accept": "application/vnd.heroku+json; version=3",
-        "Authorization": "Basic #{ENV["HEROKU_API_KEY"]}"
-      }
+    heroku = PlatformAPI.connect_oauth(ENV["HEROKU_TOKEN"])
+    body = {
+      hostname: ctx.domain.name
     }
-    response = HTTParty.post(url, options)
-    dns_response = OpenStruct.new(response.parsed_response)
-
-    if response.success?
-      ctx.domain.dns_target = dns_response.cname
-    else
-      ctx.fail_and_return!(dns_response.message)
-    end
+    response = heroku.domain.create(ENV["HEROKU_APP_NAME"], body)
+    dns_response = OpenStruct.new(response)
+    ctx.domain.dns_target = dns_response.cname
   end
 end
