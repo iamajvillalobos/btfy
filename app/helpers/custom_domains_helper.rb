1fa0d810e18d8c01508bd9ef7307cc7366212184
AJ Villalobos <hi@ajvillalobos.com>
2020-11-08 15:54:50 +0800

    feat: don't show custom domains that are not yet active

--- app/helpers/custom_domains_helper.rb
+++ app/helpers/custom_domains_helper.rb
@@ -1,6 +1,6 @@
 module CustomDomainsHelper
   def custom_domain_dropdown
-    domains = current_user.custom_domains.map { |cd| [cd.name, cd.id] }
+    domains = current_user.custom_domains.success.map { |cd| [cd.name, cd.id] }
     domains << [ENV["DEFAULT_URL_HOST"], nil]
     domains.sort
   end
