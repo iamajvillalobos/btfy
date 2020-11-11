da588cbe48372191259819141121cb2e400b6e95
AJ Villalobos <hi@ajvillalobos.com>
2020-11-11 19:06:10 +0800

    feat: remove 7 days and 60 days filter

--- app/actions/get_link_visits.rb
+++ app/actions/get_link_visits.rb
@@ -8,14 +8,12 @@ class GetLinkVisits
 
   executed do |ctx|
     ctx.visits = case ctx.period
-    when "day"
-      ctx.link.visits.where(created_at: Date.today.all_day)
     when "7d"
       ctx.link.visits.where(created_at: 1.week.ago.midnight..Time.now)
     when "30d"
       ctx.link.visits.where(created_at: 1.month.ago.midnight..Time.now)
-    when "60d"
-      ctx.link.visits.where(created_at: 2.month.ago.midnight..Time.now)
+    else
+      ctx.link.visits.where(created_at: 1.week.ago.midnight..Time.now)
     end
   end
 end
--- app/components/period_dropdown_component.rb
+++ app/components/period_dropdown_component.rb
@@ -6,14 +6,10 @@ class PeriodDropdownComponent < ViewComponent::Base
 
   def button_name
     case params[:period]
-    when "day"
-      "Today"
     when "7d"
       "Last 7 days"
     when "30d"
       "Last 30 days"
-    when "60d"
-      "Last 60 days"
     else
       @default_text
     end
