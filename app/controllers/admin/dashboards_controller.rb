2a1dc881d8246aa6a75773241af89987da40de84
AJ Villalobos <hi@ajvillalobos.com>
2020-11-20 14:58:04 +0800

    fix: add gmt+8 timezone for admin cards

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -5,11 +5,11 @@ class Admin::DashboardsController < ApplicationController
   skip_authorization_check
 
   def show
-    @link_visits_yday = LinkVisit.where(created_at: Date.yesterday.all_day)
-    @link_visits_today = LinkVisit.where(created_at: Date.today.all_day)
-    @links_created_yday = Link.where(created_at: Date.yesterday.all_day)
-    @links_created_today = Link.where(created_at: Date.today.all_day)
-    @users_yday = User.where(created_at: Date.yesterday.all_day)
-    @users_today = User.where(created_at: Date.today.all_day)
+    @link_visits_yday = LinkVisit.where(created_at: Date.yesterday.in_time_zone("Asia/Singapore").all_day)
+    @link_visits_today = LinkVisit.where(created_at: Date.today.in_time_zone("Asia/Singapore").all_day)
+    @links_created_yday = Link.where(created_at: Date.yesterday.in_time_zone("Asia/Singapore").all_day)
+    @links_created_today = Link.where(created_at: Date.today.in_time_zone("Asia/Singapore").all_day)
+    @users_yday = User.where(created_at: Date.yesterday.in_time_zone("Asia/Singapore").all_day)
+    @users_today = User.where(created_at: Date.today.in_time_zone("Asia/Singapore").all_day)
   end
 end
