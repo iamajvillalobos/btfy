0587c92fb000747e7050015128441f00e2f15022
AJ Villalobos <hi@ajvillalobos.com>
2020-12-09 11:26:12 +0800

    fix: redirect to correct 404 url for connected domains

--- app/controllers/redirect_links_controller.rb
+++ app/controllers/redirect_links_controller.rb
@@ -3,10 +3,13 @@ class RedirectLinksController < ApplicationController
   skip_authorization_check
 
   def new
-    link = LinkFinder.call(request, params[:slug]).link
+    link_details = LinkFinder.call(request, params[:slug])
+    link = link_details.link
 
     if link
       redirect_to RedirectLink.call(link).url
+    elsif link_details.custom_domain
+      redirect_to link_details.custom_domain.not_found_url
     else
       render "errors/not_found"
     end
