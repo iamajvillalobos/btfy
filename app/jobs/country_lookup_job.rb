f57c8c6305c6b8897a5f5954c0b84c6fff2414ef
AJ Villalobos <hi@ajvillalobos.com>
2021-01-25 00:12:01 +0800

    feat: revert to ipstack for geocoding

--- app/jobs/country_lookup_job.rb
+++ app/jobs/country_lookup_job.rb
@@ -2,14 +2,9 @@ class CountryLookupJob < ApplicationJob
   queue_as :default
 
   def perform(ip, visit_id)
-    # get country details
-    url = "https://ipapi.co/#{ip}/country/"
-    result = HTTParty.get(url)
-    country_code = result.body
-    country_name = ISO3166::Country.new(country_code).name
-
-    # update link visit country
+    url = "http://api.ipstack.com/#{ip}?access_key=#{ENV['IPSTACK_API_KEY']}"
+    response = ::HTTParty.get(url)
     visit = LinkVisit.find(visit_id)
-    visit.update(country: country_name)
+    visit.update(country: response["country_name"])
   end
 end
